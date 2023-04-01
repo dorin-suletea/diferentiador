4ed95d51e8b12066e0a0c85e66f1d830671bfd1b
dorin <suleteadorin@gmail.com>
2023-04-01 22:46:46 +0200

    deleted staged

--- data/data.go
+++ data/data.go
@@ -1,27 +1,61 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
 type FileStatus int32
 
 const (
-	untacked FileStatus = iota
-	modified
-	deleted
-	statged
+	added    FileStatus = 0
+	modified FileStatus = 1
+	deleted  FileStatus = 2
 )
 
-func GetChangedFiles() {
-	fmt.Printf(runCmd("git", "status"))
+func GetChangedFiles() error {
+	files := []File{}
+
+	deletedFilesStr := runCmd("git", "status", "-s")
+	lines := splitByEmptyNewline(deletedFilesStr)
+	for _, line := range lines {
+		splits := strings.Split(line, " ")
+		if len(splits) > 2 {
+			return errors.New("InternalError line must have 2 tokens :" + line)
+		}
+		switch splits[0] {
+		case "A":
+			files = append(files, File{splits[1], true, added})
+		case "D":
+			files = append(files, File{splits[1], true, deleted})
+		case "M":
+			files = append(files, File{splits[1], true, modified})
+		}
+	}
+
+	fmt.Printf("%v", lines)
 }
+
+// only deleted
+//git ls-files -d
+
+// modified and deleted
+//git ls-files -m
+
+// all staged
+//git ls-files -s
+
+// all unstaged
+//git ls-files -o
 
 type File struct {
 	FileName string
 	staged   bool
+	status   FileStatus
 }
 
 func runCmd(cmd string, args ...string) string {
@@ -29,6 +63,16 @@ func runCmd(cmd string, args ...string) string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("The date is %s\n", out)
 	return string(out[:])
+}
+
+func splitByEmptyNewline(str string) []string {
+	strNormalized := regexp.
+		MustCompile("\r\n").
+		ReplaceAllString(str, "\n")
+
+	return regexp.
+		MustCompile(`\n\s*\n`).
+		Split(strNormalized, -1)
+
 }
