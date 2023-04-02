3a59d6279c965d8b2bdbef776a56e8c732b61bee
dorin <suleteadorin@gmail.com>
2023-04-02 14:59:40 +0200

    rows

--- main.go
+++ main.go
@@ -7,290 +7,82 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/url"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/cmd/fyne_demo/tutorials"
-	"fyne.io/fyne/v2/cmd/fyne_settings/settings"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	"github.com/dorin-suletea/diferentiador~/data"
 )
 
-func main() {
-	data.GetChangedFiles()
-}
+type myTheme struct{}
 
 //git diff --no-prefix -U1000 main.go
-func main3() {
+func main() {
 	app := app.NewWithID("diferentiador")
+	// app.Settings().SetTheme(ui.NewCustomTheme())
 	app.SetIcon(theme.FyneLogo())
 	window := app.NewWindow("diferentiador")
-
 	changedContent := widget.NewTextGridFromString("potato poteason")
 
-	var data = []string{"a", "string", "list"}
-	changedFiles := container.NewBorder(nil, nil, nil, nil, widget.NewList(
-		func() int {
-			return len(data)
-		},
-		func() fyne.CanvasObject {
-			return container.NewPadded(
-				widget.NewButton("", nil),
-			)
-		},
-		func(id widget.ListItemID, item fyne.CanvasObject) {
-			item.(*fyne.Container).Objects[0].(*widget.Button).SetText(data[id])
-			item.(*fyne.Container).Objects[0].(*widget.Button).OnTapped = func() {
-				fmt.Println("I am button " + data[id])
-				changedContent.SetText(data[id])
-			}
-		}))
+	// var data = []string{"a", "string", "list"}
 
-	split := container.NewHSplit(changedFiles, changedContent)
+	label := widget.NewLabel("AAAAAAAAA")
+	button := widget.NewButtonWithIcon(">", theme.ConfirmIcon(), func() { fmt.Println("tapped text & leading icon button") })
+	// button.Resize(fyne.NewSize(150, 30))
+	// label.Resize(fyne.NewSize(150, 30))
+
+	// grid := container.New(layout.NewGridWrapLayout(fyne.NewSize(40, 40)),
+	// 	label, widget.NewLabel(""), widget.NewLabel(""), button)
+	// grid := container.New(layout.NewGridLayout(2),
+	// 	label, button)
+	grid := container.New(layout.NewGridWrapLayout(fyne.NewSize(200, 40)),
+		container.New(layout.NewHBoxLayout(), label, layout.NewSpacer(), button),
+		container.New(layout.NewHBoxLayout(), label, layout.NewSpacer(), button))
+
+	// row := container.NewBorder(nil, nil, label, button)
+
+	// row.Resize(fyne.NewSize(150, 30))
+
+	// list := widget.NewList(
+	// 	func() int {
+	// 		return len(data)
+	// 	},
+	// 	func() fyne.CanvasObject {
+	// 		return widget.NewLabel("template")
+	// 	},
+	// 	func(i widget.ListItemID, o fyne.CanvasObject) {
+
+	// 		o.(*widget.Label).SetText(data[i])
+	// 	})
+	// list.Select(1000)
+	// changedFiles := container.NewBorder(nil, nil, nil, nil, widget.NewList(
+	// 	func() int {
+	// 		return len(data)
+	// 	},
+	// 	func() fyne.CanvasObject {
+	// 		row := container.NewCenter(
+	// 			widget.NewButton("", nil),
+	// 			widget.NewLabel("Will be replaced"))
+	// 		row.Resize(fyne.Size{row.Size().Width, 10})
+	// 		return row
+	// 	},
+	// 	func(id widget.ListItemID, item fyne.CanvasObject) {
+	// 		item.(*fyne.Container).Resize(fyne.Size{item.(*fyne.Container).Size().Width, 200})
+
+	// 		item.(*fyne.Container).Objects[0].(*widget.Button).SetText(data[id])
+	// 		item.(*fyne.Container).Objects[0].(*widget.Button).OnTapped = func() {
+	// 			fmt.Println("I am button " + data[id])
+	// 			changedContent.SetText(data[id])
+	// 		}
+	// 	}))
+
+	split := container.NewHSplit(grid, changedContent)
 	split.Offset = 0.2
 
 	window.SetContent(split)
 
 	window.Resize(fyne.NewSize(1040, 460))
 	window.ShowAndRun()
-}
-
-//
-//
-//
-//
-//
-var topWindow fyne.Window
-
-const preferenceCurrentTutorial = "currentTutorial"
-
-func main2() {
-	a := app.NewWithID("io.fyne.demo")
-	a.SetIcon(theme.FyneLogo())
-	a.Settings().SetTheme(theme.LightTheme())
-	makeTray(a)
-	w := a.NewWindow("Fyne Demo")
-	topWindow = w
-
-	w.SetMainMenu(makeMenu(a, w))
-	w.SetMaster()
-
-	content := container.NewMax()
-	title := widget.NewLabel("Component name blah")
-	intro := widget.NewLabel("An introduction would probably go\nhere, as well as a")
-	grid := widget.NewTextGridFromString("potato poteason")
-
-	intro.Wrapping = fyne.TextWrapWord
-	setTutorial := func(t tutorials.Tutorial) {
-		title.SetText(t.Title)
-		intro.SetText(t.Intro)
-
-		content.Objects = []fyne.CanvasObject{t.View(w)}
-		content.Refresh()
-	}
-
-	tutorial := container.NewBorder(
-		container.NewVBox(title, widget.NewSeparator(), intro, grid), nil, nil, nil, content)
-
-	// file tab
-	split := container.NewHSplit(makeNav(setTutorial, true), tutorial)
-	split.Offset = 0.2
-	w.SetContent(split)
-
-	w.Resize(fyne.NewSize(640, 460))
-	w.ShowAndRun()
-}
-
-func makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
-	newItem := fyne.NewMenuItem("New", nil)
-	checkedItem := fyne.NewMenuItem("Checked", nil)
-	checkedItem.Checked = true
-	disabledItem := fyne.NewMenuItem("Disabled", nil)
-	disabledItem.Disabled = true
-	otherItem := fyne.NewMenuItem("Other", nil)
-	mailItem := fyne.NewMenuItem("Mail", func() { fmt.Println("Menu New->Other->Mail") })
-	mailItem.Icon = theme.MailComposeIcon()
-	otherItem.ChildMenu = fyne.NewMenu("",
-		fyne.NewMenuItem("Project", func() { fmt.Println("Menu New->Other->Project") }),
-		mailItem,
-	)
-	fileItem := fyne.NewMenuItem("File", func() { fmt.Println("Menu New->File") })
-	fileItem.Icon = theme.FileIcon()
-	dirItem := fyne.NewMenuItem("Directory", func() { fmt.Println("Menu New->Directory") })
-	dirItem.Icon = theme.FolderIcon()
-	newItem.ChildMenu = fyne.NewMenu("",
-		fileItem,
-		dirItem,
-		otherItem,
-	)
-
-	openSettings := func() {
-		w := a.NewWindow("Fyne Settings")
-		w.SetContent(settings.NewSettings().LoadAppearanceScreen(w))
-		w.Resize(fyne.NewSize(480, 480))
-		w.Show()
-	}
-	settingsItem := fyne.NewMenuItem("Settings", openSettings)
-	settingsShortcut := &desktop.CustomShortcut{KeyName: fyne.KeyComma, Modifier: fyne.KeyModifierShortcutDefault}
-	settingsItem.Shortcut = settingsShortcut
-	w.Canvas().AddShortcut(settingsShortcut, func(shortcut fyne.Shortcut) {
-		openSettings()
-	})
-
-	cutShortcut := &fyne.ShortcutCut{Clipboard: w.Clipboard()}
-	cutItem := fyne.NewMenuItem("Cut", func() {
-		shortcutFocused(cutShortcut, w)
-	})
-	cutItem.Shortcut = cutShortcut
-	copyShortcut := &fyne.ShortcutCopy{Clipboard: w.Clipboard()}
-	copyItem := fyne.NewMenuItem("Copy", func() {
-		shortcutFocused(copyShortcut, w)
-	})
-	copyItem.Shortcut = copyShortcut
-	pasteShortcut := &fyne.ShortcutPaste{Clipboard: w.Clipboard()}
-	pasteItem := fyne.NewMenuItem("Paste", func() {
-		shortcutFocused(pasteShortcut, w)
-	})
-	pasteItem.Shortcut = pasteShortcut
-	performFind := func() { fmt.Println("Menu Find") }
-	findItem := fyne.NewMenuItem("Find", performFind)
-	findItem.Shortcut = &desktop.CustomShortcut{KeyName: fyne.KeyF, Modifier: fyne.KeyModifierShortcutDefault | fyne.KeyModifierAlt | fyne.KeyModifierShift | fyne.KeyModifierControl | fyne.KeyModifierSuper}
-	w.Canvas().AddShortcut(findItem.Shortcut, func(shortcut fyne.Shortcut) {
-		performFind()
-	})
-
-	helpMenu := fyne.NewMenu("Help",
-		fyne.NewMenuItem("Documentation", func() {
-			u, _ := url.Parse("https://developer.fyne.io")
-			_ = a.OpenURL(u)
-		}),
-		fyne.NewMenuItem("Support", func() {
-			u, _ := url.Parse("https://fyne.io/support/")
-			_ = a.OpenURL(u)
-		}),
-		fyne.NewMenuItemSeparator(),
-		fyne.NewMenuItem("Sponsor", func() {
-			u, _ := url.Parse("https://fyne.io/sponsor/")
-			_ = a.OpenURL(u)
-		}))
-
-	// a quit item will be appended to our first (File) menu
-	file := fyne.NewMenu("File", newItem, checkedItem, disabledItem)
-	device := fyne.CurrentDevice()
-	if !device.IsMobile() && !device.IsBrowser() {
-		file.Items = append(file.Items, fyne.NewMenuItemSeparator(), settingsItem)
-	}
-	main := fyne.NewMainMenu(
-		file,
-		fyne.NewMenu("Edit", cutItem, copyItem, pasteItem, fyne.NewMenuItemSeparator(), findItem),
-		helpMenu,
-	)
-	checkedItem.Action = func() {
-		checkedItem.Checked = !checkedItem.Checked
-		main.Refresh()
-	}
-	return main
-}
-
-func makeTray(a fyne.App) {
-	if desk, ok := a.(desktop.App); ok {
-		h := fyne.NewMenuItem("Hello", func() {})
-		h.Icon = theme.HomeIcon()
-		menu := fyne.NewMenu("Hello World", h)
-		h.Action = func() {
-			log.Println("System tray menu tapped")
-			h.Label = "Welcome"
-			menu.Refresh()
-		}
-		desk.SetSystemTrayMenu(menu)
-	}
-}
-
-func unsupportedTutorial(t tutorials.Tutorial) bool {
-	return !t.SupportWeb && fyne.CurrentDevice().IsBrowser()
-}
-
-func makeNav(setTutorial func(tutorial tutorials.Tutorial), loadPrevious bool) fyne.CanvasObject {
-	var data = []string{"a", "string", "list"}
-	list := widget.NewList(
-		func() int {
-			return len(data)
-		},
-		func() fyne.CanvasObject {
-			return container.NewPadded(
-				widget.NewButton("", nil),
-			)
-			// return widget.NewLabel("template")
-		},
-		func(id widget.ListItemID, item fyne.CanvasObject) {
-			item.(*fyne.Container).Objects[0].(*widget.Button).SetText(data[id])
-			item.(*fyne.Container).Objects[0].(*widget.Button).OnTapped = func() {
-				fmt.Println("I am button " + data[id])
-			}
-		})
-	return container.NewBorder(nil, nil, nil, nil, list)
-
-	// a := fyne.CurrentApp()
-
-	// tree := &widget.Tree{
-	// 		ChildUIDs: func(uid string) []string {
-	// 			return tutorials.TutorialIndex[uid]
-	// 		},
-	// 	IsBranch: func(uid string) bool {
-	// 		children, ok := tutorials.TutorialIndex[uid]
-
-	// 		return ok && len(children) > 0
-	// 	},
-	// 	CreateNode: func(branch bool) fyne.CanvasObject {
-	// 		return widget.NewLabel("Collection Widgets")
-	// 	},
-	// 	UpdateNode: func(uid string, branch bool, obj fyne.CanvasObject) {
-	// 		t, ok := tutorials.Tutorials[uid]
-	// 		if !ok {
-	// 			fyne.LogError("Missing tutorial panel: "+uid, nil)
-	// 			return
-	// 		}
-	// 		obj.(*widget.Label).SetText(t.Title)
-	// 		if unsupportedTutorial(t) {
-	// 			obj.(*widget.Label).TextStyle = fyne.TextStyle{Italic: true}
-	// 		} else {
-	// 			obj.(*widget.Label).TextStyle = fyne.TextStyle{}
-	// 		}
-	// 	},
-	// 	OnSelected: func(uid string) {
-	// 		if t, ok := tutorials.Tutorials[uid]; ok {
-	// 			if unsupportedTutorial(t) {
-	// 				return
-	// 			}
-	// 			a.Preferences().SetString(preferenceCurrentTutorial, uid)
-	// 			setTutorial(t)
-	// 		}
-	// 	},
-
-	// if loadPrevious {
-	// 	currentPref := a.Preferences().StringWithFallback(preferenceCurrentTutorial, "welcome")
-	// 	tree.Select(currentPref)
-	// }
-
-	// return container.NewBorder(nil, nil, nil, nil, tree)
-
-}
-
-func shortcutFocused(s fyne.Shortcut, w fyne.Window) {
-	switch sh := s.(type) {
-	case *fyne.ShortcutCopy:
-		sh.Clipboard = w.Clipboard()
-	case *fyne.ShortcutCut:
-		sh.Clipboard = w.Clipboard()
-	case *fyne.ShortcutPaste:
-		sh.Clipboard = w.Clipboard()
-	}
-	if focused, ok := w.Canvas().Focused().(fyne.Shortcutable); ok {
-		focused.TypedShortcut(s)
-	}
 }
