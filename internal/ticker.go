a6420a5efbf2b5f778379d18e7ed56fcbf82e1e7
dorin <suleteadorin@gmail.com>
2023-04-11 21:17:17 +0200

    ticker

---
+++ internal/ticker.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"log"
+	"time"
+)
+
+type Tick struct {
+	tag     string
+	timeSec int
+}
+type Ticker struct {
+	ticks []Tick
+}
+
+func NewSingleUseTicker() *Ticker {
+	return &Ticker{}
+}
+
+func (t *Ticker) Tick(tag string) *Ticker {
+	t.ticks = append(t.ticks, Tick{tag: tag, timeSec: int(time.Now().Unix())})
+	return t
+}
+
+func (t *Ticker) Print() {
+	for i, tick := range t.ticks {
+		prevTime := 0
+		if i != 0 {
+			prevTime = tick.timeSec
+		}
+		log.Printf("-> %d tag=%s, duration=%d", i, tick.tag, tick.timeSec-prevTime)
+	}
+}
