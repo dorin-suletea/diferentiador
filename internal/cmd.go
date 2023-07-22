9e6eeec637f0cf709cc25477d261b8c02aa220d0
dorin <suleteadorin@gmail.com>
2023-07-22 11:46:00 +0200

    comment

--- internal/cmd.go
+++ internal/cmd.go
@@ -5,6 +5,18 @@ import (
 	"os/exec"
 )
 
+// TODO : Figure out if and how to run in a foreign directory.
+func RunGit(rootPath string, args ...string) string {
+	cmd := "git"
+	pa := append([]string{"-C", rootPath}, args...)
+
+	out, err := exec.Command(cmd, pa...).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(out[:])
+}
+
 func RunCmd(cmd string, args ...string) string {
 	// TODO : bug it will try to run "HEAD^:internal/fileDiffModel.go"
 	// altho the file was moved but not commited into app/fileDiffModel.go
